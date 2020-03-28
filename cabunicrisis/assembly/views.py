e573ad7f8db14e3bf0ed4ba7c7d5984db6384c64
Shivam Sharma <mailtoshivam13@gmail.com>
2020-03-28 01:00:44 -0400

    Created homepage and urls.

--- cabunicrisis/assembly/views.py
+++ cabunicrisis/assembly/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+	#Landing page of Predictive Webserver.
+
+	#A lot of these variables are defined according to Shivam's style of coding.
+	#Please feel free to change them carefully, if required.
+	raw_html = render(request, 'homepage.html')
+	response = HttpResponse(raw_html)
+	
+	return response
--- cabunicrisis/cabunicrisis/urls.py
+++ cabunicrisis/cabunicrisis/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from assembly.views import home
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin-xyzzy/', admin.site.urls),
+    path('home/', home, name = 'home_page'),
+    path('assembly/', include('assembly.urls')),
 ]
