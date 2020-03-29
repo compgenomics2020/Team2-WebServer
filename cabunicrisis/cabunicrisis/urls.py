cf746ffc08b20ec82a1fcd694cfc6fd50add0718
Hanchen Wang <hwang787@gatech.edu>
2020-03-28 21:17:33 -0400

    verification email

--- cabunicrisis/cabunicrisis/urls.py
+++ cabunicrisis/cabunicrisis/urls.py
@@ -27,7 +27,10 @@ urlpatterns = [
     #path('home/', home, name = 'home_page'),
     #path('assembly/', include('assembly.urls')),
     url(r'^$', core_views.home, name='home'),
-    url(r'^login/$', auth_views.LoginView, {'template_name': 'login.html'}, name='login'),
-    url(r'^logout/$', auth_views.LogoutView, {'next_page': 'login'}, name='logout'),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
     url(r'^signup/$', core_views.signup, name='signup'),
+    url(r'^account_activation_sent/$', core_views.account_activation_sent, name='account_activation_sent'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        core_views.activate, name='activate'),
 ]
