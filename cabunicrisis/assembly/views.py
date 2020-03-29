1d97795514119508b412f2cc90a066fdfceecbb8
Hanchen Wang <hwang787@gatech.edu>
2020-03-28 20:14:36 -0400

    Update views.py

--- cabunicrisis/assembly/views.py
+++ cabunicrisis/assembly/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, redirect
+from .models import User
 # Create your views here.
 
 def index(request):
@@ -21,11 +22,11 @@ def usr_view(request,usr_id):
 	
 	return HttpResponse("You are now in your homepage"% usr_id)
 
-def assemblyResult_view(request,usr_id):
+def test_index(request):
 	#Landing page of Predictive Webserver.
 
 	#A lot of these variables are defined according to Shivam's style of coding.
 	#Please feel free to change them carefully, if required.
 	
 	
-	return HttpResponse("Your assembly result are shown here"% usr_id)
+	return render(request, 'assembly/index.html', {'usr_id' : User.uuid})
