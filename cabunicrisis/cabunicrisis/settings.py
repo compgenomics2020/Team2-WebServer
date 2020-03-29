8e3db55cfd5efddece37ec437ee650ff96711c86
Hanchen Wang <hwang787@gatech.edu>
2020-03-28 20:23:26 -0400

    Update settings.py

--- cabunicrisis/cabunicrisis/settings.py
+++ cabunicrisis/cabunicrisis/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'cabunicrisis.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'mysite/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
