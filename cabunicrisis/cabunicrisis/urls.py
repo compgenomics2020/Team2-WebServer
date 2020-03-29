b04511434be97ce8f8913bb4031cff1c66b0c3b2
Hanchen Wang <hwang787@gatech.edu>
2020-03-28 22:30:50 -0400

    upload fasta file

--- cabunicrisis/cabunicrisis/urls.py
+++ cabunicrisis/cabunicrisis/urls.py
@@ -30,4 +30,9 @@ urlpatterns = [
     url(r'^login/$', auth_views.LoginView, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.LogoutView, {'next_page': 'login'}, name='logout'),
     url(r'^signup/$', core_views.signup, name='signup'),
+    url(r'^uploads/simple/$', core_views.simple_upload, name='simple_upload'),
+    url(r'^uploads/form/$', core_views.model_form_upload, name='model_form_upload'),
+    url(r'^admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
