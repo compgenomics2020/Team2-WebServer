bb74aec0203ee6a6a7222f0a2a3e032572a5db2a
Parekh_Paarth_J_pparekh32 <pparekh32@gatech.edu>
2020-04-13 06:37:05 -0400

    Handled the indentation errors

--- cabunicrisis/prediction/models.py
+++ cabunicrisis/prediction/models.py
@@ -4,44 +4,30 @@ from assembly.models import GenomeAssembly, User
 # Create your models here.
 class Input_Files(models.Model):
 	USER_INPUT = 'user_upload'
-    GENOME_INPUT = 'genome_assembly_input'
-    INPUT_CHOICES = (
-        (USER_INPUT, 'user_upload'),
-        (GENOME_INPUT, 'genome_assemby_input'),
-    )
-    type = models.CharField(
-        max_length=20,
-        choices=INPUT_CHOICES,
-    )
-	
+	GENOME_INPUT = 'genome_assembly_input'
+	INPUT_CHOICES = ((USER_INPUT, 'user_upload'),(GENOME_INPUT, 'genome_assemby_input'),)
+	type = models.CharField(max_length=20,choices=INPUT_CHOICES)
 	user = models.ForeignKey(User, related_name = "assemble_files",blank=True)
-
 	contigs_user_path= models.CharField(max_length=512,blank=True)
-	
 	contigs_file_path=models.ForeignKey(GenomeAssembly, related_name = "contig_files",blank=True)
-	
+
 	def __str__(self):
-                return (str(self.path))
+		return (str(self.path))
 
 class Blast_Results(models.Model):
 	contigs_file_path = models.ForeignKey(Input_Files, related_name = "contig_files")
 	path = models.CharField(max_length=512)
-
 	def __str__(self):
-                return (str(self.path))
+	    return (str(self.path))
 
 class Coding_Rename_Path(models.Model):
 	blast_file_path= models.ForeignKey(Input_Files, related_name = "blast_files")
-	path = models.CharField(max_length=512)
-
+	path = models.CharField(max_length=512)	
 	def __str__(self):
-                return (str(self.path))
+		return (str(self.path))
 
 class Nc_Final(models.Model):
-        contigs_file_path = models.ForeignKey(Input_Files, related_name = "contig_files")
-		path= models.CharField(max_length=512)
-
-        def __str__(self):
-                return (str(self.path))
-
-
+	contigs_file_path = models.ForeignKey(Input_Files, related_name = "contig_files")
+	path= models.CharField(max_length=512)
+	def __str__(self):
+		return (str(self.path))
