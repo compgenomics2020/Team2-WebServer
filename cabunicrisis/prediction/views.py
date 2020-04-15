378bc7c8d7016499c209f29bfa7d1f6d1409b342
PaarthParekh <paarthparekh2824@gmail.com>
2020-04-15 17:30:00 -0400

    Added UUID functionality and to store the files in the prediction app

--- cabunicrisis/prediction/views.py
+++ cabunicrisis/prediction/views.py
@@ -2,26 +2,40 @@ from django.http import HttpResponse, Http404
 from django.shortcuts import render, redirect
 from .models import Input_Files
 import uuid
+import os
 
 # Create your views here.
 
 def prediction_home(request):
         #Landing page of Gene Prediction isolated functionality.
-
         #Upload files forms.
         if request.method == 'GET':
                 raw_html = render(request, 'prediction/prediction.html')
                 response = HttpResponse(raw_html)
                 return response
-
         if request.method == 'POST':
-                user_uuid = uuid.uuid4()
-                for file in request.FILES.getlist('contigs.fasta'):
-                        with open("blah.txt", "w") as f:
-                                f.write(str(file.read()))
-                        #print(file.name)
-                return HttpResponse("Yes")
-
-
+        #Create a UUID for user.
+                user_uuid = str(uuid.uuid4())
+                #Directory names.
+                dir_data = 'prediction/data/'
+                dir_user = dir_data + user_uuid + '/'
+                dir_contig = dir_user + '/' + 'contigs.fasta/'
+                #Creating a directory for user.
+                os.mkdir(dir_user)
+                #Creating a directory for raw fastq files.
+                os.mkdir(dir_contig)
+                #Getting user's email.
+                email = request.POST['email']
+                #Get number of files.
+                number_of_files = 0
+                #Accessing and saving the files sent by user.
+                for file in request.FILES.getlist('contig-files'):
+                        with open(dir_contig + file.name, "w") as f:
+                                f.write(str(file.read()))	
+                                number_of_files+=1
+                #Create Raw files model.
+                #Run Pipeline.
+                raw_html = render(request,'prediction/prediction.html' , {'uuid_data': user_uuid, 'number_of_files': number_of_files})
+                return HttpResponse(raw_html)
 def pipeline_home(request):
         return HttpResponse("Pipeline Home Page")
