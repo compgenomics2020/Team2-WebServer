e68fef20bed26d4195bf139b2fc8f8bf114c4e01
Parekh_Paarth_J_pparekh32 <pparekh32@gatech.edu>
2020-04-15 14:42:19 -0400

    Changes made to be able to view the landing page

--- cabunicrisis/prediction/views.py
+++ cabunicrisis/prediction/views.py
@@ -1,25 +1,27 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, redirect
-from .models import User
+from .models import Input_Files
 import uuid
+
 # Create your views here.
 
 def prediction_home(request):
-	#Landing page of Genome Assembly isolated functionality.
+        #Landing page of Gene Prediction isolated functionality.
 
-	#Upload files forms.
-	raw_html = render(request, 'homepage.html')
-	response = HttpResponse(raw_html)
-	
-	return response
+        #Upload files forms.
+        if request.method == 'GET':
+                raw_html = render(request, 'prediction/prediction.html')
+                response = HttpResponse(raw_html)
+                return response
 
-def prediction_upload_files(request):
-	#Check for files.
+        if request.method == 'POST':
+                user_uuid = uuid.uuid4()
+                for file in request.FILES.getlist('contigs.fasta'):
+                        with open("blah.txt", "w") as f:
+                                f.write(str(file.read()))
+                        #print(file.name)
+                return HttpResponse("Yes")
 
-	#Create a UUID for user.
-	user_uuid = uuid.uuid4()
 
-	#Create Raw files model.
-
-	#Return the link to status page.
-	return HttpResponse("Upload files status page.")
+def pipeline_home(request):
+        return HttpResponse("Pipeline Home Page")
