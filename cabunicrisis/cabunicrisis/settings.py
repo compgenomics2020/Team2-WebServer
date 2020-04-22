62ea0c40e82cdb99d94ef6750273abb16ec7acd2
Shivam Sharma <mailtoshivam13@gmail.com>
2020-04-22 09:04:59 -0400

    Changed assembly models

--- cabunicrisis/cabunicrisis/settings.py
+++ cabunicrisis/cabunicrisis/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'deploy/static')
 
 
 MEDIA_URL = '/media/'
