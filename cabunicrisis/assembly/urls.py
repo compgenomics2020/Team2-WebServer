c4fb291de24f3409f7d73cb046a958f480969f60
Shivam Sharma <mailtoshivam13@gmail.com>
2020-04-24 14:29:05 -0400

    Introduced images in status page - assembly

--- cabunicrisis/assembly/urls.py
+++ cabunicrisis/assembly/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from django.urls import path
-from .views import assembly_home, pipeline_home
+from .views import assembly_home, pipeline_home, download_contig_files
 
 urlpatterns = [
 	path('home/', assembly_home, name = "assembly_home"),
+	path('download/', download_contig_files, name"download_contig_files")
 ]
