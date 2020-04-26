337ae47adb86ce49aeedd71ffa9852c0e98c73de
PaarthParekh <paarthparekh2824@gmail.com>
2020-04-25 23:12:36 -0400

    Changes to the models.py

--- cabunicrisis/prediction/models.py
+++ cabunicrisis/prediction/models.py
@@ -7,7 +7,7 @@ class Input_Files(models.Model):
 	GENOME_INPUT = 'genome_assembly_input'
 	INPUT_CHOICES = ((USER_INPUT, 'user_upload'),(GENOME_INPUT, 'genome_assemby_input'),)
 	type = models.CharField(max_length=32,choices=INPUT_CHOICES)
-	user = models.ForeignKey(User, related_name = "assemble_files",blank=True, on_delete=models.CASCADE)
+	user = models.ForeignKey(User, related_name = "assembled_files",blank=True, on_delete=models.CASCADE)
 	contigs_user_path= models.CharField(max_length=512,blank=True)
 	contigs_file_path=models.ForeignKey(GenomeAssembly, related_name ="input_files",blank=True, on_delete=models.CASCADE)
 
